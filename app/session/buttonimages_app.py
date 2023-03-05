d09d66e14f6a389bc8601498155b7d5b9a199bcd
debsdebsenson <flitzifling@posteo.de>
2023-03-05 20:36:22 +0100

    tried out to use events and callbacks in kivy

--- app/session/buttonimages_app.py
+++ app/session/buttonimages_app.py
@@ -2,7 +2,6 @@
 #import kivy 
 
 from kivy.app import App
-from kivy.uix.button import Button
 #from kivy.uix.image import Image
 
 # to change the kivy default settings we use this module config
@@ -19,35 +18,34 @@ from kivy.uix.button import Button
 #finish_webcam_session(cap)
 
 # This layout allows you to set relative coordinates for children.
-from kivy.uix.relativelayout import RelativeLayout
+#from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 # BoxLayout arranges children in a vertical or horizontal box.
 # or help to put the children at the desired location.
 #from kivy.uix.boxlayout import BoxLayout
 
-""" # from eye_detection import current_time_milliseconds
+from eye_detection import current_time_milliseconds
+from eye_detection import calculate_time_difference
+
+import time
+
+"""
 from eye_detection import start_camera_session
 from eye_detection import main_eye_detection
 from eye_detection import finish_webcam_session """
 
-# creating the root widget used in .kv file
-""" class Buttonimages(BoxLayout):
-    '''
-        no need to do anything here as
-        we are building things in .kv file
-    '''
-    pass """
+class SessionLayout(GridLayout):
 
-class ButtonimagesApp(App):
-    def build(self):
+    # Constructor which takes x keyword arguments as input.
+    def __init__(self, **kwargs):
+        super(SessionLayout, self).__init__(**kwargs)
+        self.cols = 2        
 
-        # creating Relativelayout
-        rl = RelativeLayout()
-  
-        # create an image a button
-        # Adding images normal.png image as button
-        # decided its position and size
-        b1 = Button(text ="Push Me !",
+        self.b1 = Button(text ="Push Me !",
                      color =(1, 0, .65, 1),
                      background_normal = '../images/placeholder1.png',
                      background_down ='../images/placeholder2.JPG',
@@ -55,30 +53,37 @@ class ButtonimagesApp(App):
                      pos_hint = {'left':1, 'center_y':.5}
                    )
 
-        b2 = Button(text ="Push Me Too !",
+        # Add an event when this button is pressed
+        self.b1.bind(on_press=self.pressed_time)
+        self.add_widget(self.b1)
+
+        self.b2 = Button(text ="Push Me Too !",
                      color =(1, 0, .65, 1),
                      background_normal = '../images/placeholder3.JPG',
                      background_down ='../images/placeholder2.JPG',
                      size_hint = (.49, .49),
                      pos_hint = {'right':1, 'center_y':.5}
                    )
+        
+        # Add an event when this button is pressed
+        self.b2.bind(on_press=self.pressed)
+        self.add_widget(self.b2)
 
-        # adding button to widget
-        rl.add_widget(b1)
-        rl.add_widget(b2)
-        #print(current_time_milliseconds())
+    def pressed(self, instance):
+        #print("Pressed")
+        #name = self.name.text
+        print("Pressed")
 
-        return rl
-
-#def test_fkt():
-#    print("this is a test")
+    def pressed_time(self, instance):
+        time_ms = current_time_milliseconds()
+        print(calculate_time_difference(time_ms + 20,time_ms))
+        time.sleep(0.1) 
+        new_time_ms = current_time_milliseconds()
+        print(calculate_time_difference(time_ms,new_time_ms))
+class ButtonimagesApp(App):
+    def build(self):
+        return SessionLayout()
 
 if __name__ == '__main__':
     app = ButtonimagesApp()
     app.run()
-    """ cap = start_camera_session()
-    main_eye_detection(cap)
-
-    # Make sure the webcam session is really finished
-    finish_webcam_session(cap) """
-    #print(current_time_milliseconds())
