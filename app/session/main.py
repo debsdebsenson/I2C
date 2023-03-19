041d19b7292bc512c147c293ca28b4925ab41d5a
debsdebsenson <flitzifling@posteo.de>
2023-03-19 20:49:27 +0100

    added a build in management system for the start screen and to get to the session management menu screen

--- app/session/main.py
+++ app/session/main.py
@@ -1,31 +1,61 @@
-import threading
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.app import App
 
-from buttonimages_app import ButtonimagesApp
-from eye_detection import (
-    finish_webcam_session,
-    main_eye_detection,
-    start_camera_session,
-)
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.clock import Clock
+
+from session_management_screen import SessionManagement
+
+class  StartScreen(FloatLayout):
+    def __init__(self, **kwargs):
+        super( StartScreen, self).__init__(**kwargs)
+
+        # Define buttons
+        self.btn1 = Button(text='Select a session')
+        self.btn2 = Button(text='Close application')
+
+        # Set the size and position of the buttons
+        self.btn1.size_hint = (0.5, 0.3)
+        self.btn1.pos_hint = {'center_x': 0.5, 'center_y': 0.7}
+        self.btn2.size_hint = (0.5, 0.3)
+        self.btn2.pos_hint = {'center_x': 0.5, 'center_y': 0.3}
+
+        # bind button callbacks
+        self.btn1.bind(on_press=self.btn1_behaviour)
+        self.btn2.bind(on_press=self.close_app)
+
+        # Add the buttons to the layout
+        self.add_widget(self.btn1)
+        self.add_widget(self.btn2)
+
+    def btn1_behaviour(self, *args):
+        # Clock.schedule_once(<action>, <delay time in s>)
+        Clock.schedule_once(self.switch_to_next_view, 0.1)
+
+    def switch_to_next_view(self, *args):
+        app.screen_manager.current = 'sessionManagement'
+
+    # Close the app
+    def close_app(self, instance):
+        App.get_running_app().stop()
+
+class I2CApp(App):
+    def build(self):
+        self.screen_manager = ScreenManager()
+        self.start_screen = StartScreen()
+        screen = Screen(name='startScreen')
+        screen.add_widget(self.start_screen)
+        self.screen_manager.add_widget(screen)
+
+        self.session_management = SessionManagement()
+        screen = Screen(name='sessionManagement')
+        screen.add_widget(self.session_management)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
 
 
-def run_eyetracking():
-    # Open the webcam first
-    cap = start_camera_session()
-
-    main_eye_detection(cap)
-
-    # Make sure the webcam session is really finished
-    finish_webcam_session(cap)
-
-
-if __name__ == "__main__":
-    # Create thread
-    button_thread = threading.Thread(target=run_eyetracking)
-
-    # Start the thread
-    button_thread.start()
-
-    app = ButtonimagesApp()
+if __name__ == '__main__':
+    app = I2CApp()
     app.run()
-
-    # button_thread.join()
