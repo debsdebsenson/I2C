f42b403f563a1cd918a39e644f518bfbbf99ea50
debsdebsenson <flitzifling@posteo.de>
2023-04-02 00:14:08 +0200

    Changed from GridLayout to BoxLayout, removed Label import, added ScrollableField to file for integration of the scrollable field menu

--- app/session/session_management_screen.py
+++ app/session/session_management_screen.py
@@ -1,13 +1,18 @@
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+from scrollable_field import ScrollableField
  
  # TBD: So far this is just a placeholder function for the actual screen
  # management class
-class SessionManagement(GridLayout):
+class SessionManagement(BoxLayout):
     def __init__(self, **var_args):
         super(SessionManagement, self).__init__(**var_args)
 
         self.cols = 1
 
         # Here the scrollable_field will be called at the end
-        self.add_widget(Label(text='Hello World'))
+        #self.add_widget(Label(text='Hello World'))
+        
+        # TBD: find solution to insert this screen here! - 1. implement minimal example, 2. expand it until it is working
+        self.scrollable_field_screen = ScrollableField()
+        self.add_widget(self.scrollable_field_screen)
