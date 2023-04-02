c66bd5323a67e4cf0f74906c547cfe6343db4e52
debsdebsenson <flitzifling@posteo.de>
2023-04-02 20:16:05 +0200

    added placeholder menu for session menu when a session is still empty inside the same file + changed class name so they can be differenciated better

--- app/session/session_menu.py
+++ app/session/session_menu.py
@@ -5,9 +5,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 
 
-class SessionMenu(FloatLayout):
+class SessionMenuRegular(FloatLayout):
     def __init__(self, **kwargs):
-        super(SessionMenu, self).__init__(**kwargs)
+        super(SessionMenuRegular, self).__init__(**kwargs)
 
         # Define buttons
         self.btn1 = Button(text='Start Session')
@@ -48,9 +48,46 @@ class SessionMenu(FloatLayout):
         #app.screen_manager.current = 'sessionManagement'
         print("Placeholder")
 
+
+class SessionMenuEmptySession(FloatLayout):
+    def __init__(self, **kwargs):
+        super(SessionMenuEmptySession, self).__init__(**kwargs)
+
+        # Define buttons
+        self.btn1 = Button(text='Design my Session')
+        self.btn2 = Button(text='Go back')
+
+        # Set the size and position of the buttons
+        self.btn1.size_hint = (0.5, 0.15)
+        self.btn1.pos_hint = {'center_x': 0.5, 'center_y': 0.6}
+        self.btn2.size_hint = (0.5, 0.15)
+        self.btn2.pos_hint = {'center_x': 0.5, 'center_y': 0.4}
+
+        self.btn1.bind(on_press=lambda instance: self.switch_to_edit_session())
+        self.btn2.bind(on_press=lambda instance: self.go_back_to_scrollable_field())
+
+        # Add the buttons to the layout
+        self.add_widget(self.btn1)
+        self.add_widget(self.btn2)
+
+    def go_back_to_scrollable_field(self):
+        print("Going back to last screen will be implemented in the future")
+
+    # Implementation of a little delay before opening the next page 
+    def start_session(self, *args):
+
+        # Clock.schedule_once(<action>, <delay time in s>)
+        Clock.schedule_once(self.switch_to_next_view, 0.1)
+
+    # Switch to the next screen view/page
+    def switch_to_edit_session(self, *args):
+        #app.screen_manager.current = 'sessionManagement'
+        print("Placeholder")
+
+
 class MyApp(App):
     def build(self):
-        return SessionMenu()
+        return SessionMenuEmptySession()
 
 
 if __name__ == '__main__':
