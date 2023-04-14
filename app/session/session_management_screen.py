f3a5417b952f972a64771f7a47456eebb4bf7c1b
debsdebsenson <flitzifling@posteo.de>
2023-04-14 22:17:14 +0200

    renaming the class to SessionManagementScrollableField

--- app/session/session_management_screen.py
+++ app/session/session_management_screen.py
@@ -3,12 +3,11 @@ from kivy.uix.boxlayout import BoxLayout
 from scrollable_field import ScrollableField
  
  # Management of the screens
-class SessionManagement(BoxLayout):
+class SessionManagementScrollableField(BoxLayout):
     def __init__(self, **var_args):
-        super(SessionManagement, self).__init__(**var_args)
+        super(SessionManagementScrollableField, self).__init__(**var_args)
 
         self.cols = 1
         
-        # TBD how to start next screen from here?
         self.scrollable_field_screen = ScrollableField()
         self.add_widget(self.scrollable_field_screen)
